1eb059e2a7d77b8b97960601ba1c187fb92d0584
aurelien <aurelien061298.ruide@gmail.com>
2021-02-21 01:06:19 +0100

    refactor: modification exo1 + exo2

--- src/question1/PileAdaptor.java
+++ src/question1/PileAdaptor.java
@@ -1,5 +1,33 @@
 package question1;
 
-public class PileAdaptor {
+public class PileAdaptor implements PileI<String> {
 
+    public StackImpl stack;
+
+    public PileAdaptor(StackImpl stackImpl) {
+        this.stack = stackImpl;
+    }
+
+    @Override
+    public void empiler(String s) {
+        if(s.equals("One object")){
+            this.stack.push(1);
+        }
+    }
+
+    @Override
+    public String depiler() {
+        this.stack.pop();
+        return null;
+    }
+
+
+    @Override
+    public boolean estVide() {
+        if(this.stack.getSize() == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/question2/ProxyVaultAccess.java
+++ src/question2/ProxyVaultAccess.java
@@ -1,5 +1,15 @@
 package question2;
 
-public class ProxyVaultAccess  {
+public class ProxyVaultAccess extends AbstractVaultAccess  {
+    private AbstractVaultAccess proxyVaultAccess = new RealVaultAccess();
 
+
+    @Override
+    public String accessVault(String key, boolean fingerprint, String password, boolean eyeScanner) {
+        if(key.equals("vault_key") && password.equals("vault_password")){
+            return proxyVaultAccess.accessVault(key, fingerprint, password, eyeScanner);
+        }else{
+            return "access_denied";
+        }
+    }
 }
